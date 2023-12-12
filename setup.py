70376d2339171636a34ba50c29a87f8d00e507ef
Matt Roberts <matthew.roberts@bestgateeng.com>
2023-12-12 16:15:59 -0500

    updated whl to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name="jadn-xml",
-    version="1.0", 
+    version="1.1", 
     packages=["jadnxml", "jadnxml.builder", "jadnxml.constants", "jadnxml.helpers", "jadnxml.utils", "jadnxml.validation"],
     install_requires=[
         "requests",
