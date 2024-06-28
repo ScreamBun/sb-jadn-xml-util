686d176d052b47e0d49156e82b47fa2d18ade17d
Matt Roberts <matthew.roberts@bestgateeng.com>
2024-06-28 15:38:45 -0400

    1.6.0

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
  
 setup(
     data_files=[('jadn_base_types', ['_data/xsd/jadn_base_types.xsd'])],
-    name="jadn-xml",
-    version="1.4.0", 
+    name="jadnxml",
+    version="1.6.0", 
     packages=["jadnxml", "jadnxml.builder", "jadnxml.constants", "jadnxml.helpers", "jadnxml.utils", "jadnxml.validation"],
     install_requires=[
         "requests",
