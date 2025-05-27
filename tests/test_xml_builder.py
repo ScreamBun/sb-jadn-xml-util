c90e6305df8a7c8c49e0309eb26c08a19f801555
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-05-27 15:36:35 -0400

    Added valid_xml_from_string to centralize libs

--- tests/test_xml_builder.py
+++ tests/test_xml_builder.py
@@ -1,4 +1,4 @@
-from jadnxml.builder.xml_builder import build_py_from_xml, build_xml_from_json
+from jadnxml.builder.xml_builder import build_py_from_xml, build_xml_from_json, valid_xml_from_string
 
 
 def test_build_xml_from_json_dict():
@@ -93,3 +93,25 @@ def test_build_py_from_xml_choice():
     json_data = build_py_from_xml(j_schema, root, xml)
     
     assert json_data != None   
+    
+
+def test_valid_xml_from_string():
+    
+    valid_xml = """<Root-Test>
+        <field_value_1 key="1">data 1</field_value_1>
+    </Root-Test>"""
+    
+    invalid_xml = """<Root-Test>
+        <field_value_1 key="1">data 1</field_value_1>
+    </Root-Testzzz>"""    
+    
+    
+    valid_result = valid_xml_from_string(valid_xml)
+    
+    try:
+        invalid_result = valid_xml_from_string(invalid_xml)
+    except ValueError as e:
+        invalid_result = None
+    
+    assert valid_result == True
+    assert invalid_result == False or invalid_result is None
